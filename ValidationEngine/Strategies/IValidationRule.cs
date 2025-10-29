c8f4717da3a26465cce1dd564e54e9977ecdcbce
shakana0 <shakanajamal3@gmail.com>
2025-10-29 10:10:42 +0100

    feat: add interface for validation rules

---
+++ ValidationEngine/Strategies/IValidationRule.cs
@@ -0,0 +1,9 @@
+﻿using ValidationEngine.Core;
+
+namespace ValidationEngine.Strategies
+{
+    public interface IValidationRule
+    {
+        ValidationResult? Validate(string propertyName, object value, Attribute attribute);
+    }
+}
