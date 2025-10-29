3d34e3f649aacaa39005754b6b5f9ff3a8411f69
shakana0 <shakanajamal3@gmail.com>
2025-10-29 10:09:23 +0100

    feat: add not empty strategy

---
+++ ValidationEngine/Strategies/NotEmptyStrategy .cs
@@ -0,0 +1,18 @@
+﻿using ValidationEngine.Core;
+
+namespace ValidationEngine.Strategies
+{
+    public class NotEmptyStrategy : IValidationRule
+    {
+        public ValidationResult? Validate(string propertyName, object value, Attribute attribute)
+        {
+            if (value is string str && string.IsNullOrWhiteSpace(str))
+            {
+                return new ValidationResult(propertyName, "Field can not be empty");
+            }
+
+            return null;
+        }
+    }
+}
+//works with [NotEmpty]
