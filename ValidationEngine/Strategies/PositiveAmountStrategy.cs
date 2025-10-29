e27ca6f526aa12206a7cd8a42f7b41f16b9e2acb
shakana0 <shakanajamal3@gmail.com>
2025-10-29 10:09:51 +0100

    feat: add positive amount strategy

---
+++ ValidationEngine/Strategies/PositiveAmountStrategy.cs
@@ -0,0 +1,17 @@
+﻿using ValidationEngine.Core;
+
+namespace ValidationEngine.Strategies
+{
+    public class PositiveAmountStrategy : IValidationRule
+    {
+        public ValidationResult? Validate(string propertyName, object value, Attribute attribute)
+        {
+            if (value is decimal amount && amount <= 0)
+            {
+                return new ValidationResult(propertyName, "Amount must be positive");
+            }
+
+            return null;
+        }
+    }
+}
