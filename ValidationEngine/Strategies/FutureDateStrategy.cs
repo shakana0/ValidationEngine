c0309bb8821b488242cab91e9f987a5d70588b20
shakana0 <shakanajamal3@gmail.com>
2025-10-29 10:08:17 +0100

    feat: add future date strategy

---
+++ ValidationEngine/Strategies/FutureDateStrategy.cs
@@ -0,0 +1,17 @@
+﻿using ValidationEngine.Core;
+
+namespace ValidationEngine.Strategies
+{
+    public class FutureDateStrategy : IValidationRule
+    {
+        public ValidationResult? Validate(string propertyName, object value, Attribute attribute)
+        {
+            if (value is DateTime date && date <= DateTime.Now)
+            {
+                return new ValidationResult(propertyName, "Date must be in the future");
+            }
+
+            return null;
+        }
+    }
+}
